74297aa8719f62a65e4ff66a51860f03f5441ad8
lizhen <hisansui@gmail.com>
2024-01-17 23:56:41 +0800

    修改未mediaplayer

--- app/src/main/java/com/lingsi/studyimage/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/lingsi/studyimage/viewmodel/MainViewModel.kt
@@ -1,42 +1,68 @@
 package com.lingsi.studyimage.viewmodel
 
+import android.R
 import android.content.Context
-import android.media.AudioAttributes
-import android.media.AudioManager
-import android.media.SoundPool
+import android.content.res.AssetFileDescriptor
+import android.media.MediaPlayer
 import androidx.annotation.RawRes
 import androidx.lifecycle.ViewModel
 
+
 class MainViewModel : ViewModel() {
-    private var soundPool: SoundPool? = null
-    private var currentSoundId: Int? = null
+    private var mediaPlayer: MediaPlayer? = null
 
     fun play(context: Context, @RawRes resId: Int) {
-        if (soundPool == null) {
-            val builder = SoundPool.Builder()
-            builder.setMaxStreams(1)
-            val attrBuilder = AudioAttributes.Builder()
-            attrBuilder.setLegacyStreamType(AudioManager.STREAM_MUSIC)
-            builder.setAudioAttributes(attrBuilder.build())
-            soundPool = builder.build()
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer()
         }
-        stop()
-        currentSoundId = soundPool?.load(context, resId, 1)
-        soundPool?.setOnLoadCompleteListener { soundPool, sampleId, status ->
-            if (status == 0) {
-                soundPool.play(currentSoundId!!, 1F, 1F, 1, 0, 1F)
-            }
-        }
+        mediaPlayer?.reset()
+        val file: AssetFileDescriptor = context.resources.openRawResourceFd(resId)
+        mediaPlayer?.setDataSource(file.fileDescriptor, file.startOffset, file.length)
+        mediaPlayer?.prepare()
+        mediaPlayer?.start()
+        file.close()
     }
 
     fun stop() {
-        soundPool?.let { pool ->
-            currentSoundId?.let { id ->
-                pool.stop(id)
-                pool.unload(id)
-                currentSoundId = null
-            }
+        if (mediaPlayer?.isPlaying == true){
+            mediaPlayer?.stop()
         }
     }
+
+    override fun onCleared() {
+        super.onCleared()
+        mediaPlayer?.release()
+    }
+
+//    private var soundPool: SoundPool? = null
+//    private var currentSoundId: Int? = null
+//
+//    fun play(context: Context, @RawRes resId: Int) {
+//        if (soundPool == null) {
+//            val builder = SoundPool.Builder()
+//            builder.setMaxStreams(1)
+//            val attrBuilder = AudioAttributes.Builder()
+//            attrBuilder.setLegacyStreamType(AudioManager.STREAM_MUSIC)
+//            builder.setAudioAttributes(attrBuilder.build())
+//            soundPool = builder.build()
+//        }
+//        stop()
+//        currentSoundId = soundPool?.load(context, resId, 1)
+//        soundPool?.setOnLoadCompleteListener { soundPool, sampleId, status ->
+//            if (status == 0) {
+//                soundPool.play(currentSoundId!!, 1F, 1F, 1, 0, 1F)
+//            }
+//        }
+//    }
+//
+//    fun stop() {
+//        soundPool?.let { pool ->
+//            currentSoundId?.let { id ->
+//                pool.stop(id)
+//                pool.unload(id)
+//                currentSoundId = null
+//            }
+//        }
+//    }
 
 }
